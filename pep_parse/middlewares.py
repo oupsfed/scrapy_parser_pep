43c3b56befde1737825a26627bc9c91e84a0f95a
oupsfed <oups.fedorov@gmail.com>
2023-06-29 16:33:56 +0300

    Поменял названия переменных

--- pep_parse/middlewares.py
+++ pep_parse/middlewares.py
@@ -5,23 +5,23 @@ class PepParseSpiderMiddleware:
 
     @classmethod
     def from_crawler(cls, crawler):
-        s = cls()
-        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
-        return s
+        middleware = cls()
+        crawler.signals.connect(middleware.spider_opened, signal=signals.spider_opened)
+        return middleware
 
     def process_spider_input(self, response, spider):
         return None
 
     def process_spider_output(self, response, result, spider):
-        for i in result:
-            yield i
+        for row in result:
+            yield row
 
     def process_spider_exception(self, response, exception, spider):
         pass
 
     def process_start_requests(self, start_requests, spider):
-        for r in start_requests:
-            yield r
+        for row in start_requests:
+            yield row
 
     def spider_opened(self, spider):
         spider.logger.info('Spider opened: %s' % spider.name)
@@ -31,9 +31,9 @@ class PepParseDownloaderMiddleware:
 
     @classmethod
     def from_crawler(cls, crawler):
-        s = cls()
-        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
-        return s
+        middleware = cls()
+        crawler.signals.connect(middleware.spider_opened, signal=signals.spider_opened)
+        return middleware
 
     def process_request(self, request, spider):
         return None
